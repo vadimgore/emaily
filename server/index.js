a4062457a6e1f85dc37cdc9c8b5d7fdefba23fc2
Vadim Gore <vadimgore@hotmail.com>
2018-10-20 21:11:35 -0700

    finished server side

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
 /*jshint esversion: 6 */
 
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({message: 'hello world!'});
-});
+app.use(
+    cookieSession({
+        // 30 days in msec
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
